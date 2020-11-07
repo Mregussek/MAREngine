2dcf33f11498aec7249a69f6913a8a89da508394
Mateusz Rzeczyca Win <mateuszrzeczyca17@gmail.com>
2020-11-08 00:13:27 +0100

    added semaphore

--- VulkanSandbox/main.cpp
+++ VulkanSandbox/main.cpp
@@ -3,6 +3,15 @@
 #include "src/Window.h"
 
 
+/*
+There is no need to include all those files in MAREngine!
+
+remote: warning: GH001: Large files detected. You may want to try Git Large File Storage - https://git-lfs.github.com.
+remote: warning: See http://git.io/iEPt8g for more information.
+remote: warning: File VulkanSandbox/3rd_party/Vulkan/Lib32/SPIRV-Tools-opt.lib is 81.30 MB; this is larger than GitHub's recommended maximum file size of 50.00 MB
+remote: warning: File VulkanSandbox/3rd_party/Vulkan/Lib32/shaderc_combined.lib is 56.33 MB; this is larger than GitHub's recommended maximum file size of 50.00 MB
+*/
+
 #define VK_CHECK(call) \
     do { \
         const VkResult result = call; \
@@ -23,7 +32,7 @@ VkInstance createVulkanInstance() {
 #else
     constexpr std::array<const char*, 1> extensions = {
 #endif
-        VK_KHR_SURFACE_EXTENSION_NAME,
+        VK_KHR_SURFACE_EXTENSION_NAME
     };
 
     VkInstanceCreateInfo createInfo{ VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO };
@@ -70,19 +79,23 @@ VkPhysicalDevice createPhysicalDevice(VkInstance instance) {
     }(); 
 }
 
-VkDevice createDevice(VkPhysicalDevice physicalDevice, uint32_t* familyIndex) {
-    *familyIndex = 0; // this may produce validation error
+VkDevice createDevice(VkPhysicalDevice physicalDevice, uint32_t familyIndex) {
+    constexpr std::array<const char*, 1> extensions{
+        VK_KHR_SWAPCHAIN_EXTENSION_NAME
+    };
 
     float queueProperties{ 1.f };
 
     VkDeviceQueueCreateInfo queueInfo{ VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO };
-    queueInfo.queueFamilyIndex = *familyIndex;
+    queueInfo.queueFamilyIndex = familyIndex;
     queueInfo.queueCount = 1;
     queueInfo.pQueuePriorities = &queueProperties;
 
     VkDeviceCreateInfo createInfo{ VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO };
     createInfo.queueCreateInfoCount = 1;
     createInfo.pQueueCreateInfos = &queueInfo;
+    createInfo.enabledExtensionCount = extensions.size();
+    createInfo.ppEnabledExtensionNames = extensions.data();
 
     VkDevice device{ 0 };
     VK_CHECK( vkCreateDevice(physicalDevice, &createInfo, nullptr, &device) );
@@ -104,32 +117,48 @@ VkSurfaceKHR createSurface(VkInstance instance, GLFWwindow* window) {
     }
 }
 
-int main(void) {
-    Window window{};
-    window.initialize("MAREngine Vulkan Renderer", 1200, 800);
-
-    uint32_t familyIndex{ 0 };
-    auto instance = createVulkanInstance();
-    auto physicalDevice = createPhysicalDevice(instance);
-    auto device = createDevice(physicalDevice, &familyIndex);
-    auto surface = createSurface(instance, window.getWindow());
+VkSwapchainKHR createSwapchain(VkDevice device, VkSurfaceKHR surface, int32_t width, int32_t height, const uint32_t* familyIndex) {
 
     VkSwapchainCreateInfoKHR createInfo{ VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR };
     createInfo.surface = surface;
     createInfo.minImageCount = 2;
     createInfo.imageFormat = VK_FORMAT_R8G8B8A8_UNORM;
     createInfo.imageColorSpace = VK_COLOR_SPACE_SRGB_NONLINEAR_KHR;
-    createInfo.imageExtent.width = window.getWidth();
-    createInfo.imageExtent.height = window.getHeight();
+    createInfo.imageExtent.width = width;
+    createInfo.imageExtent.height = height;
     createInfo.imageArrayLayers = 1;
     createInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
     createInfo.queueFamilyIndexCount = 1;
-    createInfo.pQueueFamilyIndices = &familyIndex;
+    createInfo.pQueueFamilyIndices = familyIndex;
     createInfo.presentMode = VK_PRESENT_MODE_FIFO_KHR;
 
-    VkSwapchainKHR swapChain{ 0 };
+    VkSwapchainKHR swapchain{ 0 };
 
-    VK_CHECK( vkCreateSwapchainKHR(device, &createInfo, nullptr, &swapChain) );
+    VK_CHECK( vkCreateSwapchainKHR(device, &createInfo, nullptr, &swapchain) );
+
+    return swapchain;
+}
+
+VkSemaphore createSemaphore(VkDevice device) {
+    VkSemaphoreCreateInfo createInfo{ VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO };
+
+    VkSemaphore semaphore{ 0 };
+    VK_CHECK(vkCreateSemaphore(device, &createInfo, nullptr, &semaphore));
+
+    return semaphore;
+}
+
+int main(void) {
+    Window window{};
+    window.initialize("MAREngine Vulkan Renderer", 1200, 800);
+
+    uint32_t familyIndex{ 0 }; // this may produce validation error
+    const auto instance = createVulkanInstance();
+    const auto physicalDevice = createPhysicalDevice(instance);
+    const auto device = createDevice(physicalDevice, familyIndex);
+    const auto surface = createSurface(instance, window.getWindow());
+    const auto swapchain = createSwapchain(device, surface, window.getWidth(), window.getHeight(), &familyIndex);
+    const auto semaphore = createSemaphore(device); // check 1:40:20
 
     while (window.shouldClose()) {
         window.clear();
