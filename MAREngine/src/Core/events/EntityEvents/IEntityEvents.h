bf33404beaee1c6cd991b6d77edefc6ed977a9ac
Mateusz Rzeczyca Win <mateuszrzeczyca17@gmail.com>
2020-12-31 03:52:31 +0100

    added docs to IEntityEvents.h

--- MAREngine/src/Core/events/EntityEvents/IEntityEvents.h
+++ MAREngine/src/Core/events/EntityEvents/IEntityEvents.h
@@ -27,22 +27,58 @@ namespace marengine {
 	class Entity;
 
 
+	/*
+		Base class for all Entity Events.
+	*/
 	class IEntityEvents {
 	public:
 
+		/*
+			Overrided methods should call create Entity function on Scene and invoke other needed events.
+		*/
 		virtual void onCreateEntity() const { }
+
+		/*
+			Overrided methods should call destroy Entity function on Scene and invoke other needed events.
+			Imagine situation, when user wants delete some light and renderable from scene, which are assigned to specific entity. 
+			Then he can just destroy it.
+		*/
 		virtual void onDestroyEntity(const Entity& entity) const { }
 
+		/*
+			Overrided methods should invoke all events when entity is selected.
+			Imagine situation, when user wants to pick some entity from scene, then this event can be called.
+		*/
 		virtual void onSelectedEntity(const Entity& entity) const { }
+
+		/*
+			Overrided methods should invoke all events when entity is unselected.
+			Image situation, when user has a choice, selects some entity and then decides to change selected entity.
+			On entity, which is unselected can be called this event.
+		*/
 		virtual void onUnselectedEntity(const Entity& entity) const { }
 
+		/*
+			Overrided methods should call copy Entity function on Scene and invoke other needed events.
+			There are some situation, when entity can be copied, there is possibility to create duplicates by this event.
+		*/
 		virtual void onCopyEntity(const Entity& entity) const { }
 
+		/*
+			Overrided methods should call set visible Entity function on Scene and invoke other needed events.
+			When Entity has RenderableComponent and some material, then it can be rendered. By calling this function
+			we can be sure that entity will be visible.
+		*/
 		virtual void onSetVisibleEntity(const Entity& entity) const { }
+
+		/*
+			Overrided methods should call set invisible Entity function on Scene and invoke other needed events.
+			When Entity is already visible and we want it to disappear, then we can call this event. Its job is to
+			be sure, that entity won't be visible anymore.
+		*/
 		virtual void onSetInvisibleEntity(const Entity& entity) const { }
 
 	};
-
 
 
 }
