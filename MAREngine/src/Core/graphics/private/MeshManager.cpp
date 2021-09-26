f33bd4da112ed816ae88ca2113714873f59cfd85
Mateusz Rzeczyca <info@mateuszrzeczyca.pl>
2021-09-26 11:13:52 +0200

    more logging

--- MAREngine/src/Core/graphics/private/MeshManager.cpp
+++ MAREngine/src/Core/graphics/private/MeshManager.cpp
@@ -23,6 +23,7 @@
 #include "../public/MeshManager.h"
 #include "../../filesystem/public/FileManager.h"
 #include "../../ecs/Scene.h"
+#include "../../../Logging/Logger.h"
 
 
 namespace marengine {
@@ -33,33 +34,43 @@ namespace marengine {
 
 
     void FMeshManager::reset() {
+        MARLOG_DEBUG(ELoggerType::GRAPHICS, "Resetting MeshManager...");
         getStorage()->reset();
     }
 
     void FMeshManager::updateSceneMeshData(Scene* pScene) {
+        MARLOG_TRACE(ELoggerType::GRAPHICS, "Updating scene {} all meshes data...", pScene->getName());
         auto view{ pScene->getView<CRenderable>() };
         view.each([this, pScene](entt::entity entt_entity, const CRenderable& cRenderable) {
             const Entity entity(entt_entity, pScene->getRegistry());
             updateEntityMeshData(entity);
         });
+        MARLOG_INFO(ELoggerType::GRAPHICS, "Updated scene {} all meshes data!", pScene->getName());
     }
 
     void FMeshManager::updateEntityMeshData(const Entity& entity) const {
+        const std::string& entityTag{ entity.getComponent<CTag>().tag };
+        MARLOG_TRACE(ELoggerType::GRAPHICS, "Updating entity {} mesh data...", entityTag);
+
         auto& cRenderable{ entity.getComponent<CRenderable>() };
 
         if (isThereSubstring(cRenderable.mesh.path, "Cube")) {
+            MARLOG_DEBUG(ELoggerType::GRAPHICS, "Set Cube as entity {} has cRenderable path = ", entityTag, cRenderable.mesh.path);
             cRenderable.mesh.index = g_MeshDefaultTypeIndex;
             cRenderable.mesh.type = EMeshType::CUBE;
         }
         else if (isThereSubstring(cRenderable.mesh.path, "Surface")) {
+            MARLOG_DEBUG(ELoggerType::GRAPHICS, "Set Surface as entity {} has cRenderable path = ", entityTag, cRenderable.mesh.path);
             cRenderable.mesh.index = g_MeshDefaultTypeIndex;
             cRenderable.mesh.type = EMeshType::SURFACE;
         }
         else if (isThereSubstring(cRenderable.mesh.path, "Pyramid")) {
+            MARLOG_DEBUG(ELoggerType::GRAPHICS, "Set Pyramid as entity {} has cRenderable path = ", entityTag, cRenderable.mesh.path);
             cRenderable.mesh.index = g_MeshDefaultTypeIndex;
             cRenderable.mesh.type = EMeshType::PYRAMID;
         }
         else if(FFileManager::isContainingExtension(cRenderable.mesh.path, "obj")) {
+            MARLOG_DEBUG(ELoggerType::GRAPHICS, "Trying to load new .obj file as entity {} has cRenderable path = ", entityTag, cRenderable.mesh.path);
             const FMeshProxy* pMesh{ getStorage()->isAlreadyLoaded(cRenderable) };
             if(pMesh == nullptr) {
                 pMesh = getFactory()->emplaceExternal(cRenderable.mesh.path);
@@ -67,6 +78,7 @@ namespace marengine {
             cRenderable.mesh.index = pMesh->getIndex();
             cRenderable.mesh.type = EMeshType::EXTERNAL;
         }
+        MARLOG_INFO(ELoggerType::GRAPHICS, "Updated entity {} mesh data!", entityTag);
     }
 
     FMeshStorage* FMeshManager::getStorage() const {
